9c69f841f860cde94bdcb70c9bad37bea25c1eb1
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-04-14 11:30:10 +0000

    [Update]show,edit,destroyの実装,各ページへの変遷リンクの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,33 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to root_path
+    redirect_to book_path(book.id)
   end
 
   def index
     @book = Book.new
+    @book_lists = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+
   end
 
   private
